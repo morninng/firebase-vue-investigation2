cfe286fb184552c681a3b4e88ea4b02b713e81e9
morninng <mr_morninng_star_dash@yahoo.co.jp>
2020-01-28 12:54:46 +0900

    typescript use base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,8 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    "@nuxt/typescript-build"
   ],
   /*
    ** Nuxt.js modules
@@ -56,6 +57,12 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config: any, ctx: any) {}
+  },
+
+  typescript: {
+    typeCheck: true,
+    ignoreNotFoundWarnings: true
   }
+
 }
---
+++ pages/second/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <span>second</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class Index extends Vue {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
